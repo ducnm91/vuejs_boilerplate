93dd3b736755f7e54d5c99bb400287c911f0a43b
Gao <duc91bk@gmail.com>
2021-04-20 16:03:14 +0700

    add gg recaptcha

---
+++ src/components/Form/Recaptcha.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="my-recaptcha">
+        <vue-recaptcha sitekey="6LeMYLEaAAAAAD3dig-SEysFuZcrbXGHu_atFdvb" :loadRecaptchaScript="true" ref="invisibleRecaptcha" @verify="onVerify" size="invisible">
+            <button class="btn btn-primary">Click me</button>
+        </vue-recaptcha>
+        <button class="btn btn-primary" @click="onSubmit">Click me outside</button>
+    </div>
+</template>
+<script>
+import VueRecaptcha from 'vue-recaptcha';
+export default {
+    components: { VueRecaptcha },
+    methods: {
+        onVerify: function (response) {
+            console.log('Verify: ' + response)
+            this.resetRecaptcha()
+        },
+        onSubmit: function () {
+            this.$refs.invisibleRecaptcha.execute()
+        },
+        resetRecaptcha() {
+            this.$refs.invisibleRecaptcha.reset() // Direct call reset method
+        },
+    },
+}
+</script>
+<style lang="scss">
+.grecaptcha-badge {
+    visibility: hidden;
+}
+</style>
--- src/views/Form/Index.vue
+++ src/views/Form/Index.vue
@@ -4,6 +4,7 @@
         <MultilineText />
         <PreviewImageBeforeUploaded />
         <Validator />
+        <Recaptcha />
     </div>
 </template>
 <script>
@@ -11,6 +12,7 @@ import InputText from '../../components/Form/InputText'
 import MultilineText from '../../components/Form/MultilineText'
 import PreviewImageBeforeUploaded from '../../components/Form/PreviewImageBeforeUploaded'
 import Validator from '../../components/Form/Validator'
+import Recaptcha from '../../components/Form/Recaptcha'
 
 
 export default {
@@ -18,7 +20,8 @@ export default {
         InputText,
         MultilineText,
         PreviewImageBeforeUploaded,
-        Validator
+        Validator,
+        Recaptcha
     }
 }
 </script>
